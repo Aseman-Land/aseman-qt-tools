bbda212df48d99f1890436029e1ecbb508605e67
Bardia Daneshvar <bardia.daneshvar@gmail.com>
2015-08-30 14:43:35 +0430

    Update

--- asemantools/asemanquickview.cpp
+++ asemantools/asemanquickview.cpp
@@ -54,6 +54,9 @@
 #include "asemanaudiorecorder.h"
 #include "asemanaudioencodersettings.h"
 #endif
+#if defined(Q_OS_LINUX) && defined(QT_DBUS_LIB)
+#include "asemankdewallet.h"
+#endif
 
 #include <QPointer>
 #include <QSharedPointer>
@@ -143,6 +146,9 @@ AsemanQuickView::AsemanQuickView(int options, QWindow *parent) :
     qmlRegisterType<AsemanTaskbarButton>("AsemanTools", 1,0, "TaskbarButton");
     qmlRegisterType<AsemanMapDownloader>("AsemanTools", 1,0, "MapDownloader");
     qmlRegisterType<AsemanDragArea>("AsemanTools", 1,0, "MouseDragArea");
+#if defined(Q_OS_LINUX) && defined(QT_DBUS_LIB)
+    qmlRegisterType<AsemanKdeWallet>("AsemanTools", 1,0, "KdeWallet");
+#endif
 
 #ifdef ASEMAN_SENSORS
     qmlRegisterType<AsemanSensors>("AsemanTools", 1,0, "AsemanSensors");
@@ -327,7 +333,9 @@ void AsemanQuickView::discardFocusedText()
 void AsemanQuickView::tryClose()
 {
     p->tryClose = true;
+#ifndef ASEMAN_QML_PLUGIN
     close();
+#endif
 }
 
 bool AsemanQuickView::event(QEvent *e)
